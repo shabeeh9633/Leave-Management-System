482e77b57be7fbc1e3bc8b43ec0592519f8bf9f4
shabeeh9633 <muhammedsabeeh000@gmail.com>
2026-07-26 17:12:33 +0530

    feat(users): create custom user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        EMPLOYEE = 'EMPLOYEE', 'Employee'
+        MANAGER = 'MANAGER', 'Manager'
+        HR = 'HR', 'HR'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.EMPLOYEE,
+    )
+    monthly_salary = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=5000.00
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
