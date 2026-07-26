e765b46fc12af506a344d14384b13cc5163420d6
shabeeh9633 <muhammedsabeeh000@gmail.com>
2026-07-26 23:01:42 +0530

    chore: finalize leave management system

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -85,9 +85,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "leave_management_db",
+        "USER": "postgres",
+        "PASSWORD": "9633",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
